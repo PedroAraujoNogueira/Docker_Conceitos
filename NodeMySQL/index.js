907e592441a502d1e4312ae557932a82e99f0490
PedroAraujo <pedro.nogueira@contazap.com>
2022-04-23 20:38:24 -0300

    curso finalizado

--- NodeMySQL/index.js
+++ NodeMySQL/index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = {
+    host: 'db-mysql',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+} 
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values ('Pedro 3')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (req, resp) => {
     resp.send('<h1>Parabéns, você está evoluindo!')
 })
